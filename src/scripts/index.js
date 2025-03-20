349ddd798f0e2a02c1dc6fd96338955bc81f62ac
unknown <boneislandman00@gmail.com>
2025-03-21 00:29:25 +0300

    first commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,45 @@
 import '../pages/index.css';
 import {createCard} from './card.js';
-import {initialCards} from './cardsList.js';
+import {initialCard, initialCardB, initialCardC, initialCardD} from './cardsList.js';
 
-const placesList = document.querySelector('.places__list');
+const placesList = document.querySelector('.a');
+const placesListB = document.querySelector('.b');
+const placesListC = document.querySelector('.c');
+const placesListD = document.querySelector('.d');
 
-renderCards(initialCards);
+renderCards(initialCard);
+renderCardsB(initialCardB);
+renderCardsC(initialCardC);
+renderCardsD(initialCardD);
 
 function renderCards(cards) {
   cards.forEach(cardData => {
     const cardElement = createCard(cardData);
 
     placesList.appendChild(cardElement);
+  });
+};
+
+function renderCardsB(cards) {
+  cards.forEach(cardData => {
+    const cardElement = createCard(cardData);
+
+    placesListB.appendChild(cardElement);
+  });
+};
+
+function renderCardsC(cards) {
+  cards.forEach(cardData => {
+    const cardElement = createCard(cardData);
+
+    placesListC.appendChild(cardElement);
+  });
+};
+
+function renderCardsD(cards) {
+  cards.forEach(cardData => {
+    const cardElement = createCard(cardData);
+
+    placesListD.appendChild(cardElement);
   });
 };
--- src/scripts/like.js
+++ src/scripts/like.js
@@ -1,41 +1,74 @@
-/* этот скрипт использует такие имена классов:
-✦ like-icon — для svg-иконки анимированного сердца
-✦ card__like-button — для кнопки Like рядом с иконкой
-✦ card__icon-button — для кнопки, оборачивающей иконку
-✦ card__icon-button — для кнопки, оборачивающей иконку
-✦ is-liked — для обозначения состояния лайкнутой иконки в виде сердца
-✦ button__text — для обозначения текстового элемента внутри кнопки
-Если эти классы поменять в HTML, скрипт перестанет работать. Будьте аккуратны.
-*/
+function openModal(open) {
+  open.classList.add('popup_is-animated');
+  open.classList.add('popup_is-opened');
+  document.addEventListener('keydown', escClose);
+};
 
-const likeHeartArray = document.querySelectorAll('.burger-menu__button');
-const likeButtonArray = document.querySelectorAll('.burger-menu__button');
-const iconButtonArray = document.querySelectorAll('.burger-menu__button');
+function closeModal(close) {
+  close.classList.remove('popup_is-opened');
+  document.removeEventListener('keydown', escClose);
+};
 
-iconButtonArray.forEach((iconButton, index) => {
-  iconButton.onclick = () =>
-    toggleIsLiked(likeHeartArray[index], likeButtonArray[index]);
-});
+function escClose(evt) {
+  if (evt.key === 'Escape') {
+    closeModal(document.querySelector('.popup_is-opened'));
+  };
+};
 
-likeButtonArray.forEach((button, index) => {
-  button.onclick = () => toggleIsLiked(likeHeartArray[index], button);
-});
+function initOverlayClose(popups) {
+  popups.forEach(popup => {
+    popup.addEventListener('click', (evt) => {
+      if (evt.target === popup) {
+        closeModal(popup);
+      };
+    });
+  });
+};
 
-function toggleIsLiked(heart, button) {
-  heart.classList.toggle('is-active');
-  setButtonText(heart, button);
-}
+export {openModal, closeModal, initOverlayClose};
 
-function setButtonText(heart, button) {
-  if ([...heart.classList].includes('is-active')) {
-    setTimeout(
-      () => (button.querySelector('.button__text').textContent = 'Unlike'),
-      500
-    );
-  } else {
-    setTimeout(
-      () => (button.querySelector('.button__text').textContent = 'Like'),
-      500
-    );
-  }
-}
+import {openModal, closeModal, initOverlayClose} from '../components/modal.js';
+
+const popups = document.querySelectorAll('.popup');
+const placesList = document.querySelector('.places__list');
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const editPopup = document.querySelector('.popup_type_edit');
+const addPopup = document.querySelector('.popup_type_new-card');
+
+const userName = document.querySelector('.profile__title').textContent;
+const userDescription = document.querySelector('.profile__description').textContent;
+const inputName = document.querySelector('.popup__input_type_name');
+const inputDescription = document.querySelector('.popup__input_type_description');
+
+const newCardForm = document.forms['new-place'];
+const editFormElement = document.querySelector('.popup__form');
+
+addButton.addEventListener('click', () => openModal(addPopup));
+
+initCloseButtonsListeners();
+
+initOverlayClose(popups);
+
+function initCloseButtonsListeners() {
+  const closeButtons = document.querySelectorAll('.popup__close');
+  closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const popup = button.closest('.popup');
+  
+      closeModal(popup);
+    });
+  });
+};
+
+<div class="popup popup_type_edit">
+<div class="popup__content">
+  <button type="button" class="popup__close"></button>
+  <h3 class="popup__title">Редактировать профиль</h3>
+  <form class="popup__form" name="edit-profile">
+    <input type="text" name="name" class="popup__input popup__input_type_name" placeholder="Имя" required>
+    <input type="text" name="description" class="popup__input popup__input_type_description" placeholder="Занятие" required>
+    <button type="submit" class="button popup__button">Сохранить</button>
+  </form>
+</div>
+</div>
